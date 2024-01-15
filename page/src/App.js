9264d7b476de860d1c84763091767968f4d5d309
yurimn <leeyou6757@gmail.com>
2024-01-15 16:59:44 +0900

    update

--- page/src/App.js
+++ page/src/App.js
@@ -11,19 +11,21 @@ function App() {
 			<img src={image13} id="image-13" alt="capsule-1" />
 			<img src={image12} id="image-12" alt="capsule-2" />
 			<img src={image15} id="image-15" alt="capsule-3" />
-			<img src={ellipse} id="ellipse" alt="ellipse" />
-			<div id="content">
-				<div id="description">1년 뒤 나에게 보내는 선물,</div>
-				<div id="title">
-					TIME
-					<br />
-					CAPSULE
-				</div>
-				<div id="date">2024.01.17</div>
-				<div id="coming-soon">
-					COMING
-					<br />
-					SOON
+			<div id="ellipse-container">
+				<img src={ellipse} id="ellipse" alt="ellipse" />
+				<div id="content">
+					<div id="description">1년 뒤 나에게 보내는 선물,</div>
+					<div id="title">
+						TIME
+						<br />
+						CAPSULE
+					</div>
+					<div id="date">2024.01.17</div>
+					<div id="coming-soon">
+						COMING
+						<br />
+						SOON
+					</div>
 				</div>
 			</div>
 			<div id="footer">
